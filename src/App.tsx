c60a9ae54789626808d7af7e114eff367e1613e6
crzyNking <crzyNking@users.noreply.github.com>
2026-09-11 22:48:03 +0800

    Revert "feat: Programs is now dropdown only, not a page"
    This reverts commit e725812274ba706c38f7c5836d0f239cb7032568.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ProtectedRoute } from './components/ProtectedRoute'
 import { ToastContainer } from './components/Toast'
 import { Chatbot } from './components/Chatbot'
 import { Home } from './pages/Home'
+import Programs from './pages/Programs'
 import SeniorHigh from './pages/SeniorHigh'
 import KindergartenEnrollment from './pages/enrollment/KindergartenEnrollment'
 import ElementaryEnrollment from './pages/enrollment/ElementaryEnrollment'
@@ -25,6 +26,7 @@ function App() {
         <Chatbot />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/programs" element={<Programs />} />
           <Route path="/senior-high" element={<SeniorHigh />} />
           <Route path="/enrollment/kindergarten" element={<KindergartenEnrollment />} />
           <Route path="/enrollment/elementary" element={<ElementaryEnrollment />} />
